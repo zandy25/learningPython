0ab1fb8f5986aa314febeb2c58bf31ad14b832db
Unknown <andyzam2505@gmail.com>
2017-06-28 21:36:11 -0700

    nest

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ while 1 < 2:
     if x == "bad":
         print("that sucks")
     elif x == "good":
-            print("nice")
-            elif x == "good what about yours"
-                print("great thanks for asking")
-                else x == "bad what about yours":
-                    print("my whent great and it sucks that your day was bad")
-                elif x == "horrible":
-                    print("well that sucks for you")
-            else x == "horrible and yours":
-                print("great and yours")
-        elif x == "great and horrible":
-            print("nice and bad?")
+        print("nice")
+    elif x == "good what about yours":
+        print("great thanks for asking")
+    elif x == "bad what about yours":
+        print("my whent great and it sucks that your day was bad")
+    elif x == "horrible":
+        print("well that sucks for you")
+    elif x == "horrible and yours":
+        print("great")
+    elif x == "great and horrible":
+        print("nice and bad?")
     else:
        print("sorry but i have no idea what you just said")
                  
